a136093c8cc61f97adc171252bb5552e826b92bc
JesusRestrepo <jesusrestrepo2003@gmail.com>
2022-01-04 12:08:45 -0500

    comenzando a dar estilos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import icon from './img/icon.png'
+import png from './img/png.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bg'>
+        <header className='header'>
+          <nav className='navbar'>
+            <span className='navbar-brand'><h2><img src={icon} alt="logo"/>Box</h2></span>
+          </nav>
+        </header>
+        
+        <div className='info'>
+          <div className='text'>
+            <div className='title'>
+              <h1>
+                <p>DESARROLLO</p>
+                <p>DE PAGINAS WEB</p>
+              </h1>
+            </div>
+            <div className='description'>
+              <h4>
+                <p>Comienza a invertir en tu negocio expandiendo</p>
+                <p>el alcance de tus ideas a través</p>
+                <p>de toda la web.</p>
+              </h4>
+              <div className="button">
+                <button className="button btn-outline-success btn-lg mt-2">Cotiza tu página gratis</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className='rounded'>
+            <div className='img'>
+              <img src={png}
+                alt='imagen-desarrollo'></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
